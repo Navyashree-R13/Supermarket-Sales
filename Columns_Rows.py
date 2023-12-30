1ccd9f9e8b1112b0d7d7b715f2204bbacb26e206
Navyashree <navyashree1399@gmail.com>
2023-12-30 14:55:49 +0000

    Reads columns and rows

---
+++ Columns_Rows.py
@@ -0,0 +1,23 @@
+# Get the column names of the DataFrame
+column_names = df.columns
+# Print the column names
+print(column_names)
+
+
+# Get the number of rows in the DataFrame
+num_rows = df.shape[0]
+# Print the number of rows
+print(num_rows)
+
+
+# Display the first few rows of the DataFrame for a quick overview
+print(df.head())
+
+
+# Display a concise summary of the DataFrame, including data types and memory usage
+df.info()
+
+
+# Iterating through columns and their data types in the DataFrame and printing the results
+for column, dtype in df.dtypes.items():
+    print(f"{column}: {dtype}")
