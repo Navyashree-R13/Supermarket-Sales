80b78c9cb06d0893ba535a51ca1b588de99e0de5
Navyashree <navyashree1399@gmail.com>
2023-12-30 15:30:13 +0000

    Read CSV and Process Rows and Columns

---
+++ microcourses-project/Read_CSV.py
@@ -0,0 +1,8 @@
+# Importing the Pandas library and assigning it the alias 'pd' for convenient reference
+import pandas as pd
+
+# Importing the pyplot module from the matplotlib library for data visualization
+import matplotlib.pyplot as plt
+
+# Reading a CSV file and creating a DataFrame using Pandas
+df = pd.read_csv(r"Desktop\Retail_Sales.csv")
