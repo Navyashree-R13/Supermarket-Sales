952b7fab13a1d17e89c8b49635867a0b1644668a
Navyashree-R13 <73741300+Navyashree-R13@users.noreply.github.com>
2023-12-30 15:09:58 +0000

    Delete Read_CSV.py

--- Read_CSV.py
+++
@@ -1,8 +0,0 @@
-# Importing the Pandas library and assigning it the alias 'pd' for convenient reference
-import pandas as pd
-
-# Importing the pyplot module from the matplotlib library for data visualization
-import matplotlib.pyplot as plt
-
-# Reading a CSV file and creating a DataFrame using Pandas
-df = pd.read_csv(r"Desktop\Retail_Sales.csv")
