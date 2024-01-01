31ba363f881d66567369230128e283b2bd6759d0
Navyashree <navyashree1399@gmail.com>
2024-01-01 11:59:03 +0000

    Modifing Order Date column

--- microcourses-project/OrderDate.py
+++ microcourses-project/OrderDate.py
@@ -1,7 +1,7 @@
 # Converting the 'Order Date' column to datetime format using pandas to_datetime function
 df['Order Date'] = pd.to_datetime(df['Order Date'])
 
-"""Iterating through columns and their data types in the DataFrame and printing the results to check
+"""Iterating through columns and their data types in DataFrame and printing the results to check
 if the conversion of the 'Order Date' column to datetime format is successful"""
 for column, dtype in df.dtypes.items():
     print(f"{column}: {dtype}")
