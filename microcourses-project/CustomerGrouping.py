dce19a35d828b1d346a10a7bddfcae1fd6fd3b7a
Navyashree <navyashree1399@gmail.com>
2024-01-01 11:21:41 +0000

    Customer Grouping

--- microcourses-project/CustomerGrouping.py
+++ microcourses-project/CustomerGrouping.py
@@ -5,7 +5,8 @@ super_group['Performance']=super_group['Sales']+super_group['Profit']
 # Sort the customers by their performance metric in descending order
 super_group = super_group.sort_values(by='Performance', ascending=False)
 
-# Print the names of the top 5 customers
+
+# Print the names of the top 5 customers.
 print("The top 5 customers are:")
 print(super_group.index[:5].tolist())
 # Print the names of the last 3 customers
